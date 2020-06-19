c1c1a5ee51a7585f705edb7925ba19b0b6a82520
Amita <agondi@stanford.edu>
2020-06-19 15:01:14 +0530

    This is my Starter project #1 with all my comments written in the code

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,33 +1,18 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// values to keep track of the number of letters typed, which quote to use. etc. Don't change these values.
 
 document.addEventListener('DOMContentLoaded',function(event){
   function toggleButton() {
-    document.getElementById("leftarrow").className = 'show'; 
-    document.getElementById("rightarrow").className = 'show'; 
+    document.getElementById("upArrow").className = 'show'; 
+    document.getElementById("downArrow").className = 'show'; 
     document.getElementById("but1").className = 'btn btn-dark btn-lg show'; 
     document.getElementById("but2").className = 'btn btn-dark btn-lg show'; 
-    document.getElementById("navbar").className = 'show'; 
 }
   // array with texts to type in typewriter
   var dataText = [ "Hello! My name is Amita Gondi.", "The whole covid-19 situation sucks. A lot.", "But that gave me an opportunity to blow the dust off of the old travel albums. Metaphorically.", "Thus, presenting", "Amita's Travel Album Circa 2001"];
   
   // type one text in the typwriter
-  // keeps calling itself until the text is finished
+  // will keep calling itself until text from array is done
   function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
+    // check if text isn't finished yet
     if (i < (text.length)) {
       // add next character to h1
      document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
